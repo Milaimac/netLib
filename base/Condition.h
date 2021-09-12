050d171bcf36d8032f9196946ed729f911511cd4
Milaimac <1099843695@qq.com>
2021-09-12 16:36:19 +0800

    condition

---
+++ base/Condition.h
@@ -0,0 +1,39 @@
+#pragma once
+#include<errno.h>
+#include<time.h>
+#include"netLib/base/Mutex.h"
+#include"netLib/base/noncopyable.h"
+#include<pthread.h>
+
+namespace netLib{
+class Condition:noncopyable{
+public:
+    explicit Condition(Mutex& mutex_):mutex(mutex_){
+        pthread_cond_init(&cond, NULL);
+    }
+
+    ~Condition(){
+        pthread_cond_destroy(&cond);
+    }
+    void wait(){
+        pthread_cond_wait(&cond, mutex.get());
+    }
+    void notify(){
+        pthread_cond_signal(&cond);
+    }
+    void notifyAll(){
+        pthread_cond_broadcast(&cond);
+    }
+
+    bool waitForSeconds(int seconds){
+        struct timespec abstime;
+        clock_gettime(CLOCK_REALTIME,&abstime);
+        abstime.tv_sec += static_cast<time_t> (seconds);
+        return ETIMEDOUT == pthread_cond_timedwait(&cond, mutex.get(), &abstime);
+    }
+
+private:
+    Mutex &mutex;
+    pthread_cond_t cond;
+};
+}
