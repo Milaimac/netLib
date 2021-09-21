ae00c9b935c135901d967c2f7f5784b4182c316b
Milaimac <1099843695@qq.com>
2021-09-21 20:11:32 +0800

    add Fmt

--- base/LogStream.cc
+++ base/LogStream.cc
@@ -81,4 +81,26 @@ LogStream& LogStream::operator<<(long double v){
     }
     return  *this;
 }
+
+template<typename  T>
+Fmt::Fmt(const char* fmt, T val){
+    static_assert(std::is_arithmetic<T>::value == true, "Must be arithmetic type");
+    length_ = snprintf(buf_, sizeof buf_, fmt, val);
+    assert(static_cast<size_t>(length_) < sizeof(buf_));
+}
+
+template Fmt::Fmt(const char* fmt, char);
+
+template Fmt::Fmt(const char* fmt, short);
+template Fmt::Fmt(const char* fmt, unsigned short);
+template Fmt::Fmt(const char* fmt, int);
+template Fmt::Fmt(const char* fmt, unsigned int);
+template Fmt::Fmt(const char* fmt, long);
+template Fmt::Fmt(const char* fmt, unsigned long);
+template Fmt::Fmt(const char* fmt, long long);
+template Fmt::Fmt(const char* fmt, unsigned long long);
+
+template Fmt::Fmt(const char* fmt, float);
+template Fmt::Fmt(const char* fmt, double);
+
 } // namespace netLib
