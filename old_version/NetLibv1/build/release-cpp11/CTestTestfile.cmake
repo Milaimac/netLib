345194888b350d6c0588aa10304f38ee7f29396c
Milaimac <1099843695@qq.com>
2021-12-09 22:32:25 +0800

    del

--- old_version/NetLibv1/build/release-cpp11/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kafier/workspace/NetLib
-# Build directory: /home/kafier/workspace/NetLib/build/release-cpp11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("netLib/base")
-subdirs("netLib/net")
--- old_version/NetLibv1/build/release-cpp11/netLib/net/CMakeFiles/netLib_net.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/libnetLib_net.a"
-)
